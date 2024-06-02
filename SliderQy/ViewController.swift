a11064109e8eebd52cd4ffdbfb22dbbcd79a1bec
Kuralay19 <kairatjankizik@gmail.com>
2024-06-02 12:34:45 +0500

    init

---
+++ SliderQy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderQy
+//
+//  Created by Smart Castle M1A2004 on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
